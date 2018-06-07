a26ff1eeaa4bff5ce4e129aecc72d562bf1065b1
Marie Malarme <malarmemarie@gmail.com>
2018-06-07 18:55:57 +0200

    adding mocks files and making the get route to read the json

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const fs = require('fs')
+const util = require('util')
+const path = require('path')
 const app = express()
-const db = require('./db')
+// const db = require('./db')
+// const articles = require('./mocks/articles.json')
+const readFile = util.promisify(fs.readFile)
+const filePath = path.join(__dirname, './mocks/articles.json')
 
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', request.headers.origin)
@@ -10,9 +16,17 @@ app.use((request, response, next) => {
   next()
 })
 
-app.get('/article', (request, response) => {
-  db.getArticle()
-    .then(article => response.json(article))
+app.get('/article', (request, response, next) => {
+  readFile(filePath, 'utf8')
+  .then(data => {
+    const articles = JSON.parse(data)
+    response.json(articles)
+  })
+  .catch(next)
+
+  // readFile(filePath, 'utf8')
+  //   .then(JSON.parse)
+  //   .then(article => response.json(article))
 })
 
 app.listen(3003, () => console.log('Oh oui, je suis connecté au port 3003!!! YEAH!!! C\'est tellement bon!! C\'est une expérience incomparable!!'))
